281c042b0d4eb934d08867ac9c11935e666f6589
Jim Olson <jim.olson.456@gmail.com>
2023-06-06 10:42:59 -0500

    project blog

---
+++ src/main/java/com/codeup/codeupspringblog/KotlinSpringBlogApplication.java
@@ -0,0 +1,14 @@
+
+package com.codeup.codeupspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class codeupSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(codeupSpringBlogApplication.class, args);
+    }
+
+}
--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,5 +1,48 @@
+
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class PostController {
 
+    @GetMapping("/posts")
+
+    public String viewPosts(Model model) {
+        List<Post> posts= new ArrayList<>();
+        posts.add(new Post("first post", "this is my first post"));
+        posts.add(new Post("second post", "this is my second post"));
+
+        model.addAttribute("posts",posts);
+        return "posts/index";
+    }
+
+
+    @GetMapping("/posts/{id}")
+    public String singlePost(@PathVariable long id, Model model) {
+        Post post1= new Post("title", "description");
+        model.addAttribute("post", post1);
+        return "posts/show";
+    }
+
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String showPostForm() {
+        return "Viewing form to create a new post!";
+    }
+
+
+    @PostMapping("/posts/create")
+//    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String submitNewPost() {
+        return "submitting new post";
+    }
 }
